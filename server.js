e0c8c3a84866f702d1fa1bf21dc51fa80893b197
DanDinalandan <tang.danica04@gmail.com>
2025-09-11 15:18:06 +0800

    Updated file server features

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
@@ -14,9 +16,22 @@ app.get('/about', (req,res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'))
 });
 
-
 app.get('/contact', (req,res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'))
+});
+
+app.get('/blog', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'))
+});
+
+app.get('api/posts'm (req, res) => {
+    fs.readFile(path.join(__dirname, 'data', 'posts.json'), 'utf8', (err, data => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('Error reading blog posts');
+        }
+        res.json(JSON.rarse(data));
+    }));
 });
 
 app.listen(PORT, () => {
